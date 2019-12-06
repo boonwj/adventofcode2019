6b20aab69e67b6f6f103e77cff6c1d279a306cd1
Boon Wenjie <boonwenjie@gmail.com>
2019-12-06 21:16:30 +0800

    Add input and changes for day05 part 1

--- day05/intcode.py
+++ day05/intcode.py
@@ -4,9 +4,27 @@ This creates program that executes `intcode` programs used in Santa's space shut
 Intcode program overview:
 - A list of comma seperated integers
 - Using the following opcodes to indicate operations
-   - 1: Sum values of next 2 index's values, total stored in position indiciated by 3rd integer.
-   - 2: Multiply values of next 2 index's values, total stored in position indiciated by 3rd integer.
-   - 99: Program finish, halt.
+    - 1, parameters 3, (num1, num2, sum_position)
+        - Sum num1 and num2 and store in sum_position
+    - 2, parameters 3, (num1, num2, mul_position)
+        - Multiply num1 and num2 and store in mul_position
+    - 3, parameters 1, (input_store_position)
+        - Request user input for single int, store in input_store_position
+    - 4, parameters 1, (output_position)
+        - Print out value in output_positoin
+    - 99, parameters 0
+        - Program finish, halt.
+
+- Parameter modes are stored in the same value as instruction's opcode
+    - ABCDE
+    - DE = opcode (2 digit)
+    - ABC = parameter modes for each parameter position
+        - A = parameter 3
+        - B = parameter 2
+        - C = parameter 1
+    - Parameter modes:
+        - 1 = immediate mode (use the parameter value as it is)
+        - 0 = position mode (parameter value is a position to the requested value)
 """
 import pytest
 
@@ -15,6 +33,7 @@ test_cases = [
     ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
     ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
     ([1, 1, 1, 4, 88, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
+    ([1002, 4, 3, 4, 33], [1002, 4, 3, 4, 99]),
 ]
 
 
@@ -31,28 +50,72 @@ def read_intcode(intcode_file):
     return list(map(int, intcode))
 
 
+def process_op_code(opcode):
+    """
+    - Validate op code
+    - Determine instructions and parameter mode
+    """
+    op_mode = opcode % 100
+    para_mode = opcode // 100
+
+    param = []
+
+    if op_mode not in (1, 2, 3, 4, 99):
+        raise ValueError(f"Invalid op code: {op_mode}")
+
+    for _ in range(3):
+        param.append(para_mode % 10)
+        para_mode //= 10
+
+    return (op_mode, param)
+
 def parse_intcode(intcode):
     i = 0
 
-    def intcode_op(num1, num2, total, op):
-        num1_value = intcode[intcode[num1]]
-        num2_value = intcode[intcode[num2]]
-        total_position = intcode[total]
+    def param_value(id, mode):
+        if mode == 0:
+            return intcode[intcode[id]]
+        if mode == 1:
+            return intcode[id]
+
+    def intcode_op(index, op, param):
+        step = 0
+        if op == 1 or op == 2:
+            value_1 = param_value(index + 1, param[0])
+            value_2 = param_value(index + 2, param[1])
+            value_3 = intcode[index + 3]
+            step = 4
+        elif op == 3 or op == 4:
+            value_1 = param_value(index + 1, param[0])
+            step = 2
+
         if op == 1:
-            intcode[total_position] = num1_value + num2_value
-        if op == 2:
-            intcode[total_position] = num1_value * num2_value
+            intcode[value_3] = value_1 + value_2
+        elif op == 2:
+            intcode[value_3] = value_1 * value_2
+        elif op == 3:
+            in_int = int(input("Input single int:"))
+            if not 0 < in_int < 10:
+                raise ValueError(f"Invalid input provided: {in_int}")
+            intcode[value_1] = in_int
+        elif op == 4:
+            print(f"Output: {value_1}")
+        else:
+            raise ValueError(f"Invalid op code provided: {op}")
+
+        return step
 
     while i < len(intcode):
-        op_mode = intcode[i]
-        if op_mode not in (1, 2, 99):
-            raise ValueError(f"Invalid op code: {op_mode}")
-        elif op_mode == 1 or op_mode == 2:
-            intcode_op(i + 1, i + 2, i + 3, op_mode)
-        else:
+        print("Before:", intcode)
+        op_mode, param = process_op_code(intcode[i])
+        if op_mode == 99:
             return intcode
+        else:
+            steps = intcode_op(i, op_mode, param)
+        print(op_mode, param)
+        print(intcode)
 
-        i += 4
+        i += steps
 
 
 def intcode_executor(intcode_file, noun, verb):
@@ -74,6 +137,6 @@ def brute_force_intcode(intcode_file, expected_value):
     return None
 
 
-print(intcode_executor("./input", 12, 2))
-
-print(brute_force_intcode("./input", 19690720))
+if __name__ == "__main__":
+    print(intcode_executor("./input", 12, 2))
+    print(brute_force_intcode("./input", 19690720))
