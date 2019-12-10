84e11074c7d11083d248b859d3ef7d1b437654e3
Boon Wenjie <boonwenjie@gmail.com>
2019-12-10 15:13:52 +0800

    Add solution for day 10 part 2

--- day10/asteroid_monitor.py
+++ day10/asteroid_monitor.py
@@ -5,12 +5,13 @@ Takes in a map of asteroids in space and identify the best asteroid location to 
 """
 import math
 import sys
+import collections
 
 def get_angle(origin, location):
     o_x, o_y = origin
     l_x, l_y = location
 
-    y_diff = l_y - o_y
+    y_diff = o_y - l_y
     x_diff = l_x - o_x
 
     angle = math.degrees(math.atan2(abs(y_diff), abs(x_diff)))
@@ -26,7 +27,7 @@ def get_angle(origin, location):
     else:
         angle = 270 + angle
 
-    return angle
+    return angle % 360
 
 def get_asteroid_grid(asteroid_file):
     asteroid_grid = []
@@ -43,8 +44,8 @@ def get_asteroid_grid(asteroid_file):
 def get_distance(origin, location):
     return math.hypot(location[0] - origin[0], location[1] - origin[1])
 
-def count_seen_asteroids(ast_grid, origin):
-    ast_dist = []
+def survey_surrounding_asteroids(ast_grid, origin):
+    result = []
 
     for row in range(len(ast_grid)):
         for col in range(len(ast_grid[0])):
@@ -52,19 +53,47 @@ def count_seen_asteroids(ast_grid, origin):
                 location = (col, row)
                 dist = get_distance(origin, location)
                 angle = get_angle(origin, location)
-                ast_dist.append((dist, angle, location))
-    ast_dist = sorted(ast_dist, key=lambda x:x[0])
+                if dist != 0:
+                    result.append((dist, angle, location))
 
+    return result
+
+
+def count_seen_asteroids(ast_grid, origin):
+    ast_dist = survey_surrounding_asteroids(ast_grid, origin)
+    ast_dist = sorted(ast_dist, key=lambda x:x[0])
     count = 0
     seen_angles = {}
     for dst, angle, pos in ast_dist:
-        if dst != 0 and angle not in seen_angles:
+        if angle not in seen_angles:
             count += 1
             seen_angles[angle] = True
 
     return count
 
 
+def vaporise_asteroid_order(ast_grid, station_loc):
+    ast_profile = survey_surrounding_asteroids(ast_grid, station_loc)
+    ast_at_angles = collections.defaultdict(list)
+
+    for dst, angle, pos in ast_profile:
+        ast_at_angles[angle].append((dst, pos))
+
+    unique_angles = []
+    for angle, values in ast_at_angles.items():
+        unique_angles.append(angle)
+        ast_at_angles[angle].sort(key=lambda x: x[0])
+
+    unique_angles.sort()
+
+    destroy_order = []
+    while len(destroy_order) != len(ast_profile):
+        for angle in unique_angles:
+            if ast_at_angles[angle]:
+                _, pos = ast_at_angles[angle].pop(0)
+                destroy_order.append(pos)
+
+    return destroy_order
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -84,3 +113,5 @@ if __name__ == "__main__":
                     max_pos = origin
 
     print(max, max_pos)
+
+    destroy_order = vaporise_asteroid_order(ast_grid, max_pos)
